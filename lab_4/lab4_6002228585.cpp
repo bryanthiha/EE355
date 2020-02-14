22a52618abafdef576aa22c7c47dfbf8e3988bd5
bryanthiha <thiha@usc.edu>
2020-02-14 15:49:19 -0800

    binary

--- lab_4/lab4_6002228585.cpp
+++ lab_4/lab4_6002228585.cpp
@@ -34,25 +34,62 @@ class matrix
 		
 		void sort_row(matrix* M, int a)
 		{
-			int number = M->value[a][0];
+			int number;
 
 			int count = 0;
 
-			
-				for(int j = 1; j < 10; j++)
+			while(count < 7)
+			{	
+				number = M->value[a][0];
+
+				for(int j = 0; j < 9; j++)
 				{
 					if(M->value[a][j+1] < number) 
 					{
 
-						M->value[a][j] = M->value[a][j + 1];
+						M->value[a][j] = M->value[a][j+1];
 						
 
 						M->value[a][j+1] = number;
 					}
+
+					number = M->value[a][j+1];
 				
 				}
+				count++;
+			}
 				
 		}
+
+		int binary_search(matrix Matrix, int a)
+		{
+			int first_num = Matrix.value[0][0];
+
+			int low = 0;
+			int high = 10;
+			int mid;
+
+			while(low <= high)
+			{
+				mid = (low + high)/2;
+
+				if(Matrix.value[a][mid] > first_num)
+				{
+					high = mid - 1;
+				}
+				else if(Matrix.value[a][mid] < first_num)
+				{
+					low = mid + 1;
+				}
+				else
+				{
+					return mid;
+				}
+			}
+			
+		}
+
+		
 };
 
 
@@ -61,6 +98,7 @@ int main()
 
 	matrix array;
 	int entry, z;
+	int sub_array[10];
 
 	ifstream infile;	
 	infile.open("input.txt");
@@ -78,7 +116,7 @@ int main()
 			//cout << array.value[i][j] << " ";
 		}
 
-		cout << endl;
+		//cout << endl;
 
 	}
 
@@ -90,6 +128,7 @@ int main()
 
 	for(int k = 0; k < 10; k++)
 	{
+
 		cout << array.value[z][k] << " ";
 	}
 
@@ -99,7 +138,34 @@ int main()
 
 	for(int k = 0; k < 10; k++)
 	{
+		sub_array[k] = array.value[z][k];
+
 		cout << array.value[z][k] << " ";
+
 	}
 	cout << endl;
+
+
+	// for(int k = 0; k < 10; k++)
+	// {
+
+	// 	cout << sub_array[k] << " ";
+	// }
+
+	// cout << endl;
+
+	int b = array.binary_search(array,z);
+	cout << b << endl;
 }
+
+
+
+// 391 42 536 99 683 779 718 968 661 193
+
+// 42 391 99 536 683 718 779 661 193 968
+
+// 42 99 391 536 683 718 661 193 779 968
+
+// 42 99 391 536 683 661 193 718 779 968
+
+// 42 99 391 536 661 193 683 718 779 968
