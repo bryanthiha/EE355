373b52e30883f357fdacab55b3180fe8be8fa0f0
bryanthiha <thiha@usc.edu>
2020-02-14 12:47:23 -0800

    part one done

--- lab_4/lab4_6002228585.cpp
+++ lab_4/lab4_6002228585.cpp
@@ -8,41 +8,22 @@
 
 using namespace std;
 
-class Number
-{
-	private:
-		int value;
-
-	public:
-		int get_value()
-		{
-			return value;
-		}
-
-		void set_value(int a)
-		{
-			value = a;
-		}
-
-};
 
 class matrix
 {
 
 	public:
-		Number value[20][10];
-		
+		int value[20][10];
+
 		int linear_search(matrix Matrix)
 		{
-			int first_num;
-
-			value[0][0].set_value(first_num);
+			int first_num = Matrix.value[0][0];
 
 			for(int i = 0; i < 20; i++)											//Checking every index of both arrays
 			{
 				for(int j = 0; j < 10; j++)
 				{
-					if(Matrix.value[i][j].get_value() == first_num)
+					if(Matrix.value[i][j] == first_num && (i != 0))
 					{
 						return i;
 					}
@@ -50,6 +31,28 @@ class matrix
 			}
 		}
 
+		
+		void sort_row(matrix* M, int a)
+		{
+			int number = M->value[a][0];
+
+			int count = 0;
+
+			
+				for(int j = 1; j < 10; j++)
+				{
+					if(M->value[a][j+1] < number) 
+					{
+
+						M->value[a][j] = M->value[a][j + 1];
+						
+
+						M->value[a][j+1] = number;
+					}
+				
+				}
+				
+		}
 };
 
 
@@ -70,9 +73,9 @@ int main()
 			
 			infile >> entry;
 																	// Storing all numbers into an array 
-			array.value[i][j].set_value(entry);
+			array.value[i][j] = entry;
 
-			cout << array.value[i][j].get_value() << " ";
+			//cout << array.value[i][j] << " ";
 		}
 
 		cout << endl;
@@ -84,4 +87,19 @@ int main()
 	z = array.linear_search(array);
 
 	cout << z << endl;
+
+	for(int k = 0; k < 10; k++)
+	{
+		cout << array.value[z][k] << " ";
+	}
+
+	cout << endl;
+
+	array.sort_row(&array, z);
+
+	for(int k = 0; k < 10; k++)
+	{
+		cout << array.value[z][k] << " ";
+	}
+	cout << endl;
 }
